47cad314e6ebe403b76e5c80fd1206c9f0acdd4f
andre <dr.dre212@gmail.com>
2023-11-10 00:20:32 -0500

    Added all files

---
+++ react-state-carousel-coins/carousel/src/Carousel.test.js
@@ -0,0 +1,105 @@
+import { render, fireEvent } from "@testing-library/react";
+import Carousel from "./Carousel";
+import TEST_IMAGES from "./_testCommon.js";
+
+
+it("renders without crashing", function () {
+  render(<Carousel />);
+});
+
+it("matches to snapshot", function () {
+  const { asFragment } = render(<Card />)
+  expect(asFragment()).toMatchSnapshot();
+});
+
+it("works when you click on the right arrow", function() {
+  const { container } = render(
+    <Carousel
+      photos={TEST_IMAGES}
+      title="images for testing"
+    />
+  );
+  // expect the first image to show, but not the second
+  expect(
+    container.querySelector('img[alt="testing image 1"]')
+  ).toBeInTheDocument();
+  expect(
+    container.querySelector('img[alt="testing image 2"]')
+  ).not.toBeInTheDocument();
+
+  // move forward in the carousel
+  const rightArrow = container.querySelector(".bi-arrow-right-circle");
+  fireEvent.click(rightArrow);
+
+  // expect the second image to show, but not the first
+  expect(
+    container.querySelector('img[alt="testing image 1"]')
+  ).not.toBeInTheDocument();
+  expect(
+    container.querySelector('img[alt="testing image 2"]')
+  ).toBeInTheDocument();
+});
+
+it("works when you click on the left arrow", function() {
+  const { container } = render(
+    <Carousel
+      photos={TEST_IMAGES}
+      title="images for testing"
+    />
+  );
+  // expect the first image to show, but not the second
+  expect(
+    container.querySelector('img[alt="testing image 1"]')
+  ).toBeInTheDocument();
+  expect(
+    container.querySelector('img[alt="testing image 2"]')
+  ).not.toBeInTheDocument();
+
+  // move backward in the carousel
+  const leftArrow = container.querySelector(".bi-arrow-left-circle");
+  fireEvent.click(leftArrow);
+
+  // expect the second image to show, but not the first
+  expect(
+    container.querySelector('img[alt="testing image 1"]')
+  ).not.toBeInTheDocument();
+  expect(
+    container.querySelector('img[alt="testing image 2"]')
+  ).toBeInTheDocument();
+});
+
+it("gets rid of the left arrow when on the first image", function () {
+  const { container } = render(
+    <Carousel
+      photos={TEST_IMAGES}
+      title="images for testing"
+    />
+  );
+
+  const leftArrow = container.querySelector(".bi-arrow-left-circle");
+
+  // expect the left arrow to be gone on the first image
+  expect(
+    (leftArrow && container.querySelector('img[alt="testing image 1"]'))
+  ).not.toBeInTheDocument();
+});
+
+it("gets rid of the right arrow when on the last image", function () {
+  const { container } = render(
+    <Carousel
+      photos={TEST_IMAGES}
+      title="images for testing"
+    />
+  );
+
+  // Move forward in the carousel to the last image.
+  const rightArrow = container.querySelector(".bi-arrow-right-circle");
+  fireEvent.click(rightArrow);
+  fireEvent.click(rightArrow);
+
+  // expect the right arrow to be gone on the last image
+  expect(
+    (rightArrow && container.querySelector('img[alt="testing image 2"]'))
+    ).not.toBeInTheDocument();
+  
+})
